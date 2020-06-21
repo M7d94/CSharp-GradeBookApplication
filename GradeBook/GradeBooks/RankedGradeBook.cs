99a7a985cae319124ab5c29c812af3fdaf0de377
M7d94 <mohammed.salman.q3@gmail.com>
2020-06-21 18:58:24 +0300

    1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,7 +9,7 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type = Enums.GradeBookType.Standard;
+            Type = Enums.GradeBookType.Ranked;
         }
     }
 }
